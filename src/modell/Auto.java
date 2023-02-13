6c7eb0be7f8bb5d5d007c4d6d9fc3bcd0101a0ae
ForGetASD <vitayz60@gmail.com>
2023-02-13 14:00:25 +0100

    1. commit

---
+++ src/modell/Auto.java
@@ -0,0 +1,12 @@
+package modell;
+
+public class Auto extends Jarmu{
+    private boolean defekt;
+    
+    public void kereketCserel(){
+    
+    }
+    public boolean halad(){
+        return true;
+    }
+}
---
+++ src/modell/Jarmu.java
@@ -0,0 +1,12 @@
+package modell;
+
+public abstract class Jarmu {
+    private boolean beinditva, uzemanyag, megerkezett;
+    
+    public void beindit(){
+    
+    }
+    public void leallit(){
+    
+    }
+}
